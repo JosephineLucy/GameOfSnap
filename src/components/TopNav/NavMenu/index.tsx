6be0e96c7276ee5728cd9b7196f7fe37d41a59cc
JosephineLucy <josephine.lucyrita@gmail.com>
2026-07-05 16:28:13 +0100

    Add titles and swap icons for nav bar

--- src/components/TopNav/NavMenu/index.tsx
+++ src/components/TopNav/NavMenu/index.tsx
@@ -1,21 +1,21 @@
 import {
-  faBars,
-  faMagnifyingGlass,
-  faUser,
+  faArrowRotateRight,
+  faCog,
+  faListUl,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function NavMenu() {
   return (
     <div className="nav-menu">
-      <div className="nav-menu-item-wrapper">
-        <FontAwesomeIcon className="nav-menu-item" icon={faBars} />
+      <div className="nav-menu-item-wrapper" title="Restart game">
+        <FontAwesomeIcon icon={faArrowRotateRight} />
       </div>
-      <div className="nav-menu-item-wrapper">
-        <FontAwesomeIcon className="nav-menu-item" icon={faMagnifyingGlass} />
+      <div className="nav-menu-item-wrapper" title="Game history">
+        <FontAwesomeIcon icon={faListUl} />
       </div>
-      <div className="nav-menu-item-wrapper">
-        <FontAwesomeIcon className="nav-menu-item" icon={faUser} />
+      <div className="nav-menu-item-wrapper" title="Settings">
+        <FontAwesomeIcon icon={faCog} />
       </div>
     </div>
   );
